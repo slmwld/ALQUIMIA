ed46e76d986d3adbb6e8888f7fcf22e71719bb9c
slmwld <slmwld>
2019-02-11 21:55:34 -0600

    primer commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,15 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private _datacards: any;
 
   constructor(public navCtrl: NavController) {
+
+    this. _datacards = [
+      {title: "Aquimia Pasteleria Boutique", image: "logo-alquimia_v2.png", descripcion: "Lorem."},
+      {title: "Pastelerias Gurmet, y alta calidad", image: "890126.jpg", descripcion: "Lorem."},
+      {title: "Elaboradis con productos de alta calidad ", image: "DSC_8841.jpg", descripcion: "Lorem."}
+    ];
 
   }
 
